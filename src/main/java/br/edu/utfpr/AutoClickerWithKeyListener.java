019820e2f2480408e8a305140a8a982a4e568454
eimmig <eduardo-immig@hotmail.com>
2023-08-28 13:41:11 -0300

    Ajustado estrutura classes e removido pasta .idea

--- src/main/java/br/edu/utfpr/AutoClickerWithKeyListener.java
+++ src/main/java/br/edu/utfpr/AutoClickerWithKeyListener.java
@@ -42,34 +42,3 @@ public class AutoClickerWithKeyListener {
     }
 }
 
-class RobotClicker implements Runnable {
-    private final int targetX;
-    private final int targetY;
-
-    public RobotClicker(int targetX, int targetY) {
-        this.targetX = targetX;
-        this.targetY = targetY;
-    }
-
-    @Override
-    public void run() {
-        try {
-            Robot robot = new Robot();
-            int i = 0;
-            robot.delay(5000);
-            while (AutoClickerWithKeyListener.isRunning) {
-                robot.mouseMove(targetX, targetY);
-                robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
-                robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
-                robot.delay(20);
-                i++;
-
-                if (i == 50) {
-                    AutoClickerWithKeyListener.isRunning = false;
-                }
-            }
-        } catch (AWTException e) {
-            e.printStackTrace();
-        }
-    }
-}
